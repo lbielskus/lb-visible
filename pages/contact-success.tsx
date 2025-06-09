fb8e1c8f2cba2abc391f9127876d5bc5ba23df8b
LBielskus <liudbiel@gmail.com>
2025-06-09 14:03:14 +0300

    commit

--- pages/contact-success.tsx
+++ pages/contact-success.tsx
@@ -10,8 +10,8 @@ export default function ContactSuccessPage() {
         title='Success | LB Visible'
         description='Form submitted successfully'
       />
-      <div className='min-h-screen flex flex-col items-center justify-center px-4 text-center'>
-        <h1 className='text-3xl font-bold text-green-600 mb-4'>Thank You!</h1>
+      <div className='min-h-screen flex flex-col items-center justify-center px-6 text-center '>
+        <h1 className='text-3xl font-bold text-primary mb-4'>Thank You!</h1>
         <p className='text-md text-gray-700 mb-2'>We received your message.</p>
         <p className='text-md text-gray-600'>
           We’ll contact you shortly with answers and a customized offer.
