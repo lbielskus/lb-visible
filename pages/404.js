0f8cc1864c36222627751c2e696f1e8e42dbb2d1
LBielskus <liudbiel@gmail.com>
2025-08-06 12:03:55 +0300

    commit

--- pages/404.js
+++ pages/404.js
@@ -14,7 +14,7 @@ export default function ErrorPage() {
         >
           <h1 className='text-6xl font-bold text-primary mb-4'>404</h1>
           <h2 className='text-2xl font-semibold text-gray-700 mb-2'>
-            Page Not Found
+            Page Not Found.
           </h2>
           <p className='text-md text-gray-600'>
             Sorry, the page you are looking for does not exist.
